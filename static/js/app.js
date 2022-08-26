e049195c9a732b5b8c7e3a83e850427ef999a50a
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2022-08-26 17:20:44 +0300

    style the app

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,12 @@
 function update_dashboard(result) {
 	$("#prediction").html(result);
+	if (result == "Edible") {
+		$("#prediction").css("color", "#02b57f");
+		$("body").css("background-image", "linear-gradient(225deg, rgba(0, 245, 170, 1) 0%, rgba(146, 217, 197, 1) 20%, rgba(44, 183, 193, 1) 50%, rgba(20, 134, 166, 1) 100%)");
+	} else {
+		$("#prediction").css("color", "#cf132c");
+		$("body").css("background-image", "linear-gradient(225deg, rgba(255, 192, 69, 1) 0%, rgba(255, 157, 89, 1) 20%, rgba(240, 134, 133, 1) 50%, rgba(242, 57, 55, 1) 100%)");
+	}
 }
 
 function fetchdata() {
